96d9a27a8ed1e83bfff306bf716336a03260bfd7
tokc <eviloatmeal@eviloatmeal.com>
2018-01-04 10:59:59 +0100

    Make icon grey out on pages that are not reddit.

---
+++ background.js
@@ -0,0 +1,15 @@
+var match_rule = {
+  conditions: [
+    new chrome.declarativeContent.PageStateMatcher({
+      pageUrl: { hostEquals: 'www.reddit.com, schemes: ['https'] }
+    })
+  ],
+
+  actions: [ new chrome.declarativeContent.ShowPageAction() ]
+}
+
+chrome.runtime.onInstalled.addListener(function(details) {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([match_rules])
+  })
+})
